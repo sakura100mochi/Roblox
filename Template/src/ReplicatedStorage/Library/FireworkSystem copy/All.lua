71165c62a4547f98638ed44e29a05ca2ca13d064
平井優菜 <hiraiyuina@Y-2.local>
2025-04-25 11:52:19 +0900

    firework all update

--- Template/src/ReplicatedStorage/Library/FireworkSystem copy/All.lua
+++ Template/src/ReplicatedStorage/Library/FireworkSystem copy/All.lua
@@ -5,28 +5,22 @@ local All = {}
 --Function Name	:AutoSystem_type1
 --Explain		:全種類のタイプの花火を自動でたくさん打ち上げる
 --					2秒に1度、1~3個のランダムな花火を打ち上げる
---Arguments| Start_CFrame	: (CFrame) 花火を打ち上げる場所のCFrame
+--Arguments| Launcher	: (Part) 花火を打ち上げる発射台のパーツ
 --Return Value	: none
-function All.AutoSystem_type1(Start_CFrame)
+function All.AutoSystem_type1(Launcher)
 	local firework = require(game.ReplicatedStorage.Shared.Library["FireworkSystem copy"])
-	local Colors_Table = {"Li", "Na", "K", "Rb", "Cs", "Ca", "Sr", "Ba", "Cu", "C", "Al", "Mg"}
 	while true do
 		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local Color2 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(150, 300) / 100
-			local tmp = math.random(250, 350)
-			local ExplodeSpeed = NumberRange.new(tmp, tmp + 20)
-			local Velocity = Vector3.new(math.random(-1, 1), 2, math.random(-1, 1)) * 20
 			local ft_table = {
-				function()firework.Botan.launch(Start_CFrame, Color1, ExplodeTime, ExplodeSpeed)end,
-				function()firework.Ring.launch(Start_CFrame, Color1, ExplodeTime, ExplodeSpeed)end,
-				function()firework.UFO.launch(Start_CFrame, Color1, Color2, ExplodeTime, ExplodeSpeed)end,
-				function()firework.Kamuro.launch(Start_CFrame, Color1, ExplodeTime + 1.5)end,
-				function()firework.Toranoo.launch(Start_CFrame, Color1, ExplodeTime, Velocity)end,
-				function()firework.Kiku.launch(Start_CFrame, Color1, ExplodeTime, NumberRange.new(tmp))end
+				firework.Botan.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.Ring.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.UFO.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.Kamuro.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.Toranoo.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.Kiku.new({Parent = Launcher, Start_CFrame = Launcher.CFrame}),
+				firework.Gerb.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
 			}
-			task.spawn(function()ft_table[math.random(1, #ft_table)]()end)
+			task.spawn(function()ft_table[math.random(1, #ft_table)]:launchRandom()end)
 		end
 		task.wait(2)
 	end
@@ -34,61 +28,37 @@ end
 
 --Function Name	:AutoSystem_type2
 --Explain		:全種類のタイプの花火を自動でたくさん打ち上げる
---					2秒に1度、1~3個の花火を、タイプごとに順番に打ち上げる
---Arguments| Start_CFrame	: (CFrame) 花火を打ち上げる場所のCFrame
+--					2秒に1度、1個の花火を、タイプごとに順番に打ち上げる
+--Arguments| Launcher	: (Part) 花火を打ち上げる発射台のパーツ
 --Return Value	: none
-function All.AutoSystem_type2(Start_CFrame)
+function All.AutoSystem_type2(Launcher)
 	local firework = require(game.ReplicatedStorage.Shared.Library["FireworkSystem copy"])
-	local Colors_Table = {"Li", "Na", "K", "Rb", "Cs", "Ca", "Sr", "Ba", "Cu", "C", "Al", "Mg"}
+	local Botan = firework.Botan.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local Ring = firework.Ring.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local UFO = firework.UFO.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local Kamuro = firework.Kamuro.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local Toranoo = firework.Toranoo.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local Kiku = firework.Kiku.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+	local Gerb = firework.Gerb.new({Parent = Launcher, Start_CFrame = Launcher.CFrame})
+
 	while true do
-		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(150, 300) / 100
-			local tmp = math.random(250, 350)
-			local ExplodeSpeed = NumberRange.new(tmp, tmp + 20)
-			task.spawn(function()firework.Botan.launch(Start_CFrame, Color1, ExplodeTime, ExplodeSpeed)end)
-		end
+		task.spawn(function()Botan:launchRandom()end)
 		task.wait(2)
-		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(150, 300) / 100
-			local tmp = math.random(250, 350)
-			local ExplodeSpeed = NumberRange.new(tmp, tmp + 20)
-			task.spawn(function()firework.Ring.launch(Start_CFrame, Color1, ExplodeTime, ExplodeSpeed)end)
-		end
+		task.spawn(function()Ring:launchRandom()end)
 		task.wait(2)
-		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local Color2 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(150, 300) / 100
-			local tmp = math.random(250, 350)
-			local ExplodeSpeed = NumberRange.new(tmp, tmp + 20)
-			task.spawn(function()firework.UFO.launch(Start_CFrame, Color1, Color2, ExplodeTime, ExplodeSpeed)end)
-		end
+		task.spawn(function()UFO:launchRandom()end)
 		task.wait(2)
-		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(300, 450) / 100
-
-			task.spawn(function()firework.Kamuro.launch(Start_CFrame, Color1, ExplodeTime)end)
-		end
+		task.spawn(function()Kamuro:launchRandom()end)
 		task.wait(2)
-		for i = 1, math.random(1, 3), 1 do
-			local Color1 = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(150, 300) / 100
-			local Velocity = Vector3.new(math.random(-1, 1), 2, math.random(-1, 1)) * 20
-
-			task.spawn(function()firework.Toranoo.launch(Start_CFrame, Color1, ExplodeTime, Velocity)end)
-		end
+		task.spawn(function()Toranoo:launchRandom()end)
 		task.wait(2)
-		task.spawn(function()firework.Toranoo.fan(Start_CFrame)end)
+		task.spawn(function()Toranoo:fan()end)
 		task.wait(2)
-		for i = 1, math.random(1, 2), 1 do
-			local Color = firework.Colors[Colors_Table[math.random(1, #Colors_Table)]]
-			local ExplodeTime = math.random(200, 300) / 100
-			local ExplodeSpeed = NumberRange.new(math.random(250, 300))
-			task.spawn(function()firework.Kiku.launch(Start_CFrame, Color, ExplodeTime, ExplodeSpeed)end)
-		end
+		task.spawn(function()Kiku:launchRandom()end)
+		task.wait(2)
+		task.spawn(function()Gerb:launchRandom()end)
+		task.wait(2)
+		task.spawn(function()Gerb:fan()end)
 		task.wait(2)
 	end
 end
